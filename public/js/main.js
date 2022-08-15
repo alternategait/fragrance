724f68280f19d2c81bba2ddeae4c900d85555e98
Brandi <Brandi@Brandis-MacBook-Air.local>
2022-08-15 16:09:59 -0400

    functional delete thanks to rascal_two

--- public/js/main.js
+++ public/js/main.js
@@ -3,7 +3,6 @@ const deleteBtn = document.querySelectorAll('.fa-trash') // setting variable usi
 // const itemCompleted = document.querySelectorAll('.item span.completed') // setting variable using ".item span.completed" as selected items querySelectorAll allows for each to be targeted
 
 Array.from(deleteBtn).forEach((element, i)=>{ //???
-    element.setAttribute("id", `del${i}`)
     element.addEventListener('click', deleteItem) // use event listener to run deleteItem function line 17 in the case of a click
 })
 
@@ -16,7 +15,7 @@ Array.from(deleteBtn).forEach((element, i)=>{ //???
 // })
 
 async function deleteItem(){ //async await syntax for deleteItem function called by click on line 6
-    const itemText = this.parentNode.nextElementSibling.childNode.data // we click on the span, the LI is the parent , look to the parent and then go to the child node which is (the same span we clicked). anything that takes up space counts in the dom (?) so the text is childnode[1]?? creating a variable that seeks out a parent node and then the second (index [1] in an array) child node and using it's inner text
+    const itemText = this.parentNode.nextElementSibling.childNodes[1].innerText // we click on the span, the LI is the parent , look to the parent and then go to the child node which is (the same span we clicked). anything that takes up space counts in the dom (?) so the text is childnode[1]?? creating a variable that seeks out a parent node and then the second (index [1] in an array) child node and using it's inner text
     console.log(itemText)
     try{ //first try what follows
         const response = await fetch('deleteItem', { // response to await fetching the information that follows
@@ -28,7 +27,7 @@ async function deleteItem(){ //async await syntax for deleteItem function called
           })
         const data = await response.json() // data is the JSON delivered to the varable response on line 20
         console.log(data) // tell me that data in the console
-        // location.reload() // reload the page
+        location.reload() // reload the page
 
     }catch(err){ //if promise fails
         console.log(err) // tell me why in the console
--- server.js
+++ server.js
@@ -1,73 +1,57 @@
 const express = require('express')
 const app = express()
-// tells the server to use express syntax which allows easier building and fun middleware
 const MongoClient = require('mongodb').MongoClient
-// establishes MongoClient using mongo database for connection to our database later
 const PORT = 2121
-// tells local host where to listen
 require('dotenv').config()
-// sets up the place to keep secrets
 
 
 let db,
-    dbConnectionStr = process.env.DB_STRING, // tells us that the connection string is stored in the place of secrets
-    dbName = 'fragrance-products' // the name of our data base collection
+    dbConnectionStr = process.env.DB_STRING, 
+    dbName = 'fragrance-products' // the name of our data base the collection to access later is fragrance-safe-products
 
-MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true }) //promise syntax asking to connect using mongo client (line 4) using the connection string (line 13), using unified tolology design as node driver has seven toplogy classes 
-    .then(client => { //what to do once promise is fulfilled
-        console.log(`Connected to ${dbName} Database`) // let us know in console that we are connected to the database
-        db = client.db(dbName) // reassign db variable 
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true }) 
+    .then(client => { 
+        console.log(`Connected to ${dbName} Database`) 
+        db = client.db(dbName) 
     })
-//the following things are middleware!
-app.set('view engine', 'ejs') // middleware allowing use of ejs
-app.use(express.static('public')) //telling app/express to use public folder to serve any static files
-app.use(express.urlencoded({ extended: true })) // telling app/express how to understand incoming queries that are formatted as URL
-app.use(express.json()) // telling app/express to format objects as JSON
 
+app.set('view engine', 'ejs') 
+app.use(express.static('public')) 
+app.use(express.urlencoded({ extended: true })) 
+app.use(express.json()) 
 
-app.get('/', async (request, response) => {  //promise syntax for a request from the root route
+app.get('/', async (request, response) => {  
     const safeProducts = await db.collection('fragrance-safe-products').find().toArray()
-    console.log(safeProducts) // a variable awating items in the database collection and creating an array
-    // const itemsLeft = await db.collection('todos').countDocuments({ completed: false }) 
-// a varraible taking I assume counting the items in the database collection with completed of false
-    response.render('index.ejs', { products: safeProducts}) //once promise is fulfilled use ejs to render a page using the previous two varriables. I believe this line is replacing what follows
-    // db.collection('todos').find().toArray()
-    // .then(data => {  data indicates the array that was made
-    //     db.collection('todos').countDocuments({completed: false})
-    //     .then(itemsLeft => {
-    //         response.render('index.ejs', { items: data, left: itemsLeft }) //pass objects and array holding object into the ejs template to be rendered we are now naming that array "items"
-    //     })
-    // })
-    //.catch(error => console.error(error))
+
+    response.render('index.ejs', { products: safeProducts})
 })
 
-    app.get('/add', async (request, response) => {
-            try {
-                const safeProducts = await db.collection('fragrance-safe-products').find().toArray()
-                response.render('add.ejs', {products: safeProducts})
-            } catch (error) {
-                response.status(500).send({message: error.message})
-            }
-        }) 
+app.get('/add', async (request, response) => {
+        try {
+            const safeProducts = await db.collection('fragrance-safe-products').find().toArray()
+            response.render('add.ejs', {products: safeProducts})
+        } catch (error) {
+            response.status(500).send({message: error.message})
+        }
+    }) 
 
-        app.post("/fragrance-safe-products", (req, res) => {
-            db.collection('fragrance-safe-products').insertOne(req.body)
-                .then(result => {
-                    res.redirect('/add')
-                })
-                .catch(error => console.error (error))
+app.post("/fragrance-safe-products", (req, res) => {
+    db.collection('fragrance-safe-products').insertOne(req.body)
+        .then(result => {
+            res.redirect('/add')
         })
+        .catch(error => console.error (error))
+})
 
 
 
-app.delete('/deleteItem', (request, response) => { // a promise syntax for deleting an item
-    db.collection('todos').deleteOne({ thing: request.body.itemFromJS }) // withthin db as defined line 19, within collection todos delete the item that matches field thing identified as itemFromJS
-        .then(result => { //once promise is fulfilled
-            console.log('Todo Deleted') // tell us in the console it is deleted
-            response.json('Todo Deleted')// Provide a JSON object telling us it is deleted send it back to clint JS as part of fetch
+app.delete('/deleteItem', (request, response) => { 
+    db.collection('fragrance-safe-products').deleteOne({ name: request.body.itemFromJS }) // withth in db as defined line 19, within collection fragrance-safe-products delete the item that matches field name identified as itemFromJS
+        .then(result => { 
+            console.log('product removed') 
+            response.json('product removed')
         })
-        .catch(error => console.error(error)) // if the promise fails, let us know and why
-
+        .catch(error => console.error(error)) 
 })
 
 app.listen(process.env.PORT || PORT, () => { //to run the server use the port defined by PORT variable listed in the place of secrets (env) or PORT method provided by a hosting service like heroku
