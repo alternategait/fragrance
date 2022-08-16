305ceee67167273d77bdb6468937246e72e3e175
Brandi <Brandi@Brandis-MacBook-Air.local>
2022-08-16 15:27:21 -0400

    tidied comments and deploying to heroku

--- public/js/main.js
+++ public/js/main.js
@@ -1,42 +1,38 @@
 const deleteBtn = document.querySelectorAll('.fa-trash') 
 const likesBtn = document.querySelectorAll('.fa-heart')
 
-Array.from(deleteBtn).forEach((element, i)=>{
-    element.addEventListener('click', deleteItem) // use event listener to run deleteItem function line 17 in the case of a click
+Array.from(deleteBtn).forEach((element)=>{
+    element.addEventListener('click', deleteItem)
 })
 
-Array.from(likesBtn).forEach((element)=>{//???
-    element.addEventListener('click', addLike) // use event listener to run markComplete function  line 36 in the case of a click
+Array.from(likesBtn).forEach((element)=>{
+    element.addEventListener('click', addLike) 
 })
 
-// Array.from(itemCompleted).forEach((element)=>{//???
-//     element.addEventListener('click', markUnComplete) // use event listener to run markUnComplete function line 55 in the case of a click
-// })
 
-async function deleteItem(){ //async await syntax for deleteItem function called by click on line 6
-    const itemText = this.parentNode.nextElementSibling.childNodes[1].innerText // we click on the span, the LI is the parent , look to the parent and then go to the child node which is (the same span we clicked). anything that takes up space counts in the dom (?) so the text is childnode[1]?? creating a variable that seeks out a parent node and then the second (index [1] in an array) child node and using it's inner text
+
+async function deleteItem(){ 
+    const itemText = this.parentNode.nextElementSibling.childNodes[1].innerText 
     console.log(itemText)
-    try{ //first try what follows
-        const response = await fetch('deleteItem', { // response to await fetching the information that follows
-            method: 'delete',  // html method
-            headers: {'Content-Type': 'application/json'}, // ???
-            body: JSON.stringify({ // the body of this will contain JSON object which then needs to be converted to a string
-              'itemFromJS': itemText // this is what is delivered back and named in server.js line 87
+    try{ 
+        const response = await fetch('deleteItem', { 
+            method: 'delete',  
+            headers: {'Content-Type': 'application/json'}, 
+            body: JSON.stringify({ 
+                'itemFromJS': itemText 
             })
-          })
-        const data = await response.json() // data is the JSON delivered to the varable response on line 20
-        console.log(data) // tell me that data in the console
-        location.reload() // reload the page
-
-    }catch(err){ //if promise fails
-        console.log(err) // tell me why in the console
+            })
+        const data = await response.json() 
+        console.log(data) 
+        location.reload() 
+    }catch(err){ 
+        console.log(err) 
     }
 }
- async function addLike(){
+
+async function addLike(){
 
     const productName = this.parentNode.nextElementSibling.childNodes[1].innerText
-//     const sName = this.parentNode.childNodes[1].innerText
-//     const bName = this.parentNode.childNodes[3].innerText
     const tLikes = Number(this.parentNode.childNodes[0].innerText)
     console.log(tLikes)
     try{
@@ -56,41 +52,3 @@ async function deleteItem(){ //async await syntax for deleteItem function called
         console.log(err)
     }
 }
-// }
-// async function markComplete(){  // async await syntax for markComplete function called by click on line 9
-//     const itemText = this.parentNode.childNodes[1].innerText // ?? creating a variable that seeks out a parent node and then the second (index [1] in an array) child node and using it's inner text
-//     try{ //first try what follows
-//         const response = await fetch('markComplete', { // response to await fetching the information that follows
-//             method: 'put', // html method for updating
-//             headers: {'Content-Type': 'application/json'},// ???
-//             body: JSON.stringify({ // the body of this will contain JSON object which then needs to be converted to a string
-//                 'itemFromJS': itemText  // this is what is delivered back and named in server.js line 63
-//             })
-//           })
-//         const data = await response.json() // data is the JSON delivered to the variable response on line 39
-//         console.log(data) // tell me that data in the console
-//         location.reload() // reload the page
-
-//     }catch(err){ // if the promise fails
-//         console.log(err) // tell me why in the console
-//     }
-// }
-
-// async function markUnComplete(){ // async await syntax for markUnComplete function called by click on line 13
-//     const itemText = this.parentNode.childNodes[1].innerText // ?? creating a variable that seeks out a parent node and then the second (index [1] in an array) child node and using it's inner text
-//     try{ //first try what follows
-//         const response = await fetch('markUnComplete', { // response to await fetching the information that follows
-//             method: 'put', // html method for updating 
-//             headers: {'Content-Type': 'application/json'}, // ??? 
-//             body: JSON.stringify({ // the body of this will contain JSON object which then needs to be converted to a string
-//                 'itemFromJS': itemText // this is what is delivered back and named in server.js line 80
-//             })
-//           })
-//         const data = await response.json() // data is the JSON delivered to the variable response on line 58 
-//         console.log(data) // tell me the data in te console
-//         location.reload() // reload the page
-
-//     }catch(err){ // if the promise fails
-//         console.log(err) // tell me why in the console
-//     }
-// }
--- server.js
+++ server.js
@@ -15,6 +15,7 @@ MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
         db = client.db(dbName) 
     })
 
+//this is middleware
 app.set('view engine', 'ejs') 
 app.use(express.static('public')) 
 app.use(express.urlencoded({ extended: true })) 
@@ -25,7 +26,7 @@ app.get('/', async (request, response) => {
 
     response.render('index.ejs', { products: safeProducts})
 })
-
+//"protected" render with form
 app.get('/add', async (request, response) => {
         try {
             const safeProducts = await db.collection('fragrance-safe-products').find().toArray()
@@ -34,7 +35,7 @@ app.get('/add', async (request, response) => {
             response.status(500).send({message: error.message})
         }
     }) 
-
+// posting from form
 app.post("/fragrance-safe-products", (req, res) => {
     db.collection('fragrance-safe-products').insertOne(req.body)
         .then(result => {
@@ -43,6 +44,7 @@ app.post("/fragrance-safe-products", (req, res) => {
         .catch(error => console.error (error))
 })
 
+// adding likes from index page
 app.put('/addOneLike', (request, response) => {
     db.collection('fragrance-safe-products').updateOne({ name: request.body.name },{
         $set: {
@@ -50,17 +52,16 @@ app.put('/addOneLike', (request, response) => {
             }
     },{
         sort: {_id: -1},
-        upsert: true
+        upsert: true //allow express/mongo to add new field if it doesn't exist.
     })
     .then(result => {
-        console.log('Added One Like')
         response.json('Like Added')
     })
     .catch(error => console.error(error))
 
 })
 
-
+// deleting
 app.delete('/deleteItem', (request, response) => { 
     db.collection('fragrance-safe-products').deleteOne({ name: request.body.itemFromJS }) // withth in db as defined line 19, within collection fragrance-safe-products delete the item that matches field name identified as itemFromJS
         .then(result => { 
@@ -70,6 +71,6 @@ app.delete('/deleteItem', (request, response) => {
         .catch(error => console.error(error)) 
 })
 
-app.listen(process.env.PORT || PORT, () => { //to run the server use the port defined by PORT variable listed in the place of secrets (env) or PORT method provided by a hosting service like heroku
-    console.log(`Server running on port ${PORT}`) // tell us in the console that it is running and define which port. Is this bad practice to say if we want to obscure the port in .env?
+app.listen(process.env.PORT || PORT, () => { 
+    console.log(`Server running on port ${PORT}`) 
 })
